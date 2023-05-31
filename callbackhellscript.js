5a49cd20dfea13fdbc38f6ecfd5dc631ee140aff
Pavithran280798 <130635078+Pavithran280798@users.noreply.github.com>
2023-05-31 16:15:00 +0530

    day-17

---
+++ callbackhellscript.js
@@ -0,0 +1,44 @@
+setTimeout(() => {
+    const letter = document.querySelector(".timer");
+    letter.innerText = 10;
+    setTimeout(() => {
+      const letter = document.querySelector(".timer");
+      letter.innerText = 9;
+      setTimeout(() => {
+        const letter = document.querySelector(".timer");
+        letter.innerText = 8;
+        setTimeout(() => {
+          const letter = document.querySelector(".timer");
+          letter.innerText = 7;
+          setTimeout(() => {
+            const letter = document.querySelector(".timer");
+            letter.innerText = 6;
+            setTimeout(() => {
+              const letter = document.querySelector(".timer");
+              letter.innerText = 5;
+              setTimeout(() => {
+                const letter = document.querySelector(".timer");
+                letter.innerText = 4;
+                setTimeout(() => {
+                  const letter = document.querySelector(".timer");
+                  letter.innerText = 3;
+                  setTimeout(() => {
+                    const letter = document.querySelector(".timer");
+                    letter.innerText = 2;
+                    setTimeout(() => {
+                      const letter = document.querySelector(".timer");
+                      letter.innerText = 1;
+                      setTimeout(() => {
+                        const letter = document.querySelector(".timer");
+                        letter.innerText = "Happy Independence Day";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
